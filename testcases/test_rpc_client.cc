c768572aa6e5c867ce803a259668d09e3c99e30e
huangli999 <chandlebing@gmail.com>
2025-03-26 17:40:50 +0800

    rpc

--- testcases/test_rpc_client.cc
+++ testcases/test_rpc_client.cc
@@ -9,34 +9,32 @@
 #include <memory>
 #include <unistd.h>
 #include <google/protobuf/service.h>
-#include "rocket/common/log.h"
-#include "rocket/common/config.h"
-#include "rocket/common/log.h"
-#include "rocket/net/tcp/tcp_client.h"
-#include "rocket/net/tcp/net_addr.h"
-#include "rocket/net/coder/string_coder.h"
-#include "rocket/net/coder/abstract_protocol.h"
-#include "rocket/net/coder/tinypb_coder.h"
-#include "rocket/net/coder/tinypb_protocol.h"
-#include "rocket/net/tcp/net_addr.h"
-#include "rocket/net/tcp/tcp_server.h"
-#include "rocket/net/rpc/rpc_dispatcher.h"
-#include "rocket/net/rpc/rpc_controller.h"
-#include "rocket/net/rpc/rpc_channel.h"
-#include "rocket/net/rpc/rpc_closure.h"
+#include "hl/common/log.h"
+#include "hl/common/config.h"
+#include "hl/common/log.h"
+#include "hl/net/tcp/tcp_client.h"
+#include "hl/net/tcp/net_addr.h"
+#include "hl/net/coder/string_coder.h"
+#include "hl/net/coder/abstract_protocol.h"
+#include "hl/net/coder/tinypb_coder.h"
+#include "hl/net/coder/tinypb_protocol.h"
+#include "hl/net/tcp/net_addr.h"
+#include "hl/net/tcp/tcp_server.h"
+#include "hl/net/rpc/rpc_dispatcher.h"
+#include "hl/net/rpc/rpc_controller.h"
+// #include "hl/net/rpc/rpc_channel.h"
+#include "hl/net/rpc/rpc_closure.h"
 
 #include "order.pb.h"
 
-
-
 void test_tcp_client() {
 
-  rocket::IPNetAddr::s_ptr addr = std::make_shared<rocket::IPNetAddr>("127.0.0.1", 12345);
-  rocket::TcpClient client(addr);
+  hl::IPNetAddr::s_ptr addr = std::make_shared<hl::IPNetAddr>("127.0.0.1", 12346);
+  hl::TcpClient client(addr);
   client.connect([addr, &client]() {
     DEBUGLOG("conenct to [%s] success", addr->toString().c_str());
-    std::shared_ptr<rocket::TinyPBProtocol> message = std::make_shared<rocket::TinyPBProtocol>();
-    message->m_msg_id = "99998888";
+    std::shared_ptr<hl::TinyPBProtocol> message = std::make_shared<hl::TinyPBProtocol>();
+    message->m_req_id = "99998888";
     message->m_pb_data = "test pb data";
 
     makeOrderRequest request;
@@ -44,72 +42,73 @@ void test_tcp_client() {
     request.set_goods("apple");
     
     if (!request.SerializeToString(&(message->m_pb_data))) {
-      ERRORLOG("serilize error");
+      ERRORLOG("serilize error",NULL);
       return;
     }
 
     message->m_method_name = "Order.makeOrder";
 
-    client.writeMessage(message, [request](rocket::AbstractProtocol::s_ptr msg_ptr) {
+    client.writeMessage(message, [request](hl::AbstractProtocol::s_ptr msg_ptr) {
       DEBUGLOG("send message success, request[%s]", request.ShortDebugString().c_str());
     });
 
 
-    client.readMessage("99998888", [](rocket::AbstractProtocol::s_ptr msg_ptr) {
-      std::shared_ptr<rocket::TinyPBProtocol> message = std::dynamic_pointer_cast<rocket::TinyPBProtocol>(msg_ptr);
-      DEBUGLOG("msg_id[%s], get response %s", message->m_msg_id.c_str(), message->m_pb_data.c_str());
+    client.readMessage("99998888", [](hl::AbstractProtocol::s_ptr msg_ptr) {
+      std::shared_ptr<hl::TinyPBProtocol> message = std::dynamic_pointer_cast<hl::TinyPBProtocol>(msg_ptr);
+      DEBUGLOG("msg_id[%s], get response %s", message->m_req_id.c_str(), message->m_pb_data.c_str());
       makeOrderResponse response;
 
       if(!response.ParseFromString(message->m_pb_data)) {
-        ERRORLOG("deserialize error");
+        ERRORLOG("deserialize error",NULL);
         return;
       }
-      DEBUGLOG("get response success, response[%s]", response.ShortDebugString().c_str());
+
+      DEBUGLOG("get response success, response[%s]", response.order_id().c_str());
     });
   });
 }
 
-void test_rpc_channel() {
+// void test_rpc_channel() {
 
-  NEWRPCCHANNEL("127.0.0.1:12345", channel);
+//   NEWRPCCHANNEL("127.0.0.1:12345", channel);
 
-  // std::shared_ptr<makeOrderRequest> request = std::make_shared<makeOrderRequest>();
+//   // std::shared_ptr<makeOrderRequest> request = std::make_shared<makeOrderRequest>();
 
-  NEWMESSAGE(makeOrderRequest, request);
-  NEWMESSAGE(makeOrderResponse, response);
+//   NEWMESSAGE(makeOrderRequest, request);
+//   NEWMESSAGE(makeOrderResponse, response);
 
-  request->set_price(100);
-  request->set_goods("apple");
+//   request->set_price(100);
+//   request->set_goods("apple");
 
-  NEWRPCCONTROLLER(controller);
-  controller->SetMsgId("99998888");
-  controller->SetTimeout(10000);
+//   NEWRPCCONTROLLER(controller);
+//   controller->SetMsgId("99998888");
+//   controller->SetTimeout(10000);
 
-  std::shared_ptr<rocket::RpcClosure> closure = std::make_shared<rocket::RpcClosure>(nullptr, [request, response, channel, controller]() mutable {
-    if (controller->GetErrorCode() == 0) {
-      INFOLOG("call rpc success, request[%s], response[%s]", request->ShortDebugString().c_str(), response->ShortDebugString().c_str());
-      // 执行业务逻辑
-      if (response->order_id() == "xxx") {
-        // xx
-      }
-    } else {
-      ERRORLOG("call rpc failed, request[%s], error code[%d], error info[%s]", 
-        request->ShortDebugString().c_str(), 
-        controller->GetErrorCode(), 
-        controller->GetErrorInfo().c_str());
-    }
+//   std::shared_ptr<hl::RpcClosure> closure = std::make_shared<hl::RpcClosure>(nullptr, [request, response, channel, controller]() mutable {
+//     if (controller->GetErrorCode() == 0) {
+//       INFOLOG("call rpc success, request[%s], response[%s]", request->ShortDebugString().c_str(), response->ShortDebugString().c_str());
+//       // 执行业务逻辑
+//       if (response->order_id() == "xxx") {
+//         // xx
+//       }
+//     } else {
+//       ERRORLOG("call rpc failed, request[%s], error code[%d], error info[%s]", 
+//         request->ShortDebugString().c_str(), 
+//         controller->GetErrorCode(), 
+//         controller->GetErrorInfo().c_str());
+//     }
   
-    INFOLOG("now exit eventloop");
-    // channel->getTcpClient()->stop();
-    channel.reset();
-  });
+//     INFOLOG("now exit eventloop");
+//     // channel->getTcpClient()->stop();
+//     channel.reset();
+//   });
   
-  {
-    std::shared_ptr<rocket::RpcChannel> channel = std::make_shared<rocket::RpcChannel>(rocket::RpcChannel::FindAddr("127.0.0.1:12345"));
-    ;
-    channel->Init(controller, request, response, closure);
-    Order_Stub(channel.get()).makeOrder(controller.get(), request.get(), response.get(), closure.get());
-  }
+//   {
+//     std::shared_ptr<hl::RpcChannel> channel = std::make_shared<hl::RpcChannel>(hl::RpcChannel::FindAddr("127.0.0.1:12345"));
+//     ;
+//     channel->Init(controller, request, response, closure);
+//     Order_Stub(channel.get()).makeOrder(controller.get(), request.get(), response.get(), closure.get());
+//   }
 
   // CALLRPRC("127.0.0.1:12345", Order_Stub, makeOrder, controller, request, response, closure);
 
@@ -117,18 +116,17 @@ void test_rpc_channel() {
 
   // xxx
   // 协程
-}
+// }
 
 int main() {
 
-  rocket::Config::SetGlobalConfig(NULL);
+  hl::Config::SetGlobalConfig("/home/hl/hl-tinyrpc/conf/hl.xml");
+  hl::Logger::InitGlobalLogger();
 
-  rocket::Logger::InitGlobalLogger(0);
+  test_tcp_client();
+  // test_rpc_channel();
 
-  // test_tcp_client();
-  test_rpc_channel();
-
-  INFOLOG("test_rpc_channel end");
+  INFOLOG("test_rpc_channel end",NULL);
 
   return 0;
 }
--- testcases/test_rpc_server.cc
+++ testcases/test_rpc_server.cc
@@ -9,18 +9,18 @@
 #include <memory>
 #include <unistd.h>
 #include <google/protobuf/service.h>
-#include "rocket/common/log.h"
-#include "rocket/common/config.h"
-#include "rocket/common/log.h"
-#include "rocket/net/tcp/tcp_client.h"
-#include "rocket/net/tcp/net_addr.h"
-#include "rocket/net/coder/string_coder.h"
-#include "rocket/net/coder/abstract_protocol.h"
-#include "rocket/net/coder/tinypb_coder.h"
-#include "rocket/net/coder/tinypb_protocol.h"
-#include "rocket/net/tcp/net_addr.h"
-#include "rocket/net/tcp/tcp_server.h"
-#include "rocket/net/rpc/rpc_dispatcher.h"
+#include "hl/common/log.h"
+#include "hl/common/config.h"
+#include "hl/common/log.h"
+#include "hl/net/tcp/tcp_client.h"
+#include "hl/net/tcp/net_addr.h"
+#include "hl/net/coder/string_coder.h"
+#include "hl/net/coder/abstract_protocol.h"
+#include "hl/net/coder/tinypb_coder.h"
+#include "hl/net/coder/tinypb_protocol.h"
+#include "hl/net/tcp/net_addr.h"
+#include "hl/net/tcp/tcp_server.h"
+#include "hl/net/rpc/rpc_dispatcher.h"
 
 #include "order.pb.h"
 
@@ -30,16 +30,16 @@ class OrderImpl : public Order {
                       const ::makeOrderRequest* request,
                       ::makeOrderResponse* response,
                       ::google::protobuf::Closure* done) {
-    APPDEBUGLOG("start sleep 5s");
-    sleep(5);
-    APPDEBUGLOG("end sleep 5s");
+    // APPDEBUGLOG("start sleep 5s");
+    // sleep(5);
+    // APPDEBUGLOG("end sleep 5s");
     if (request->price() < 10) {
       response->set_ret_code(-1);
       response->set_res_info("short balance");
       return;
     }
     response->set_order_id("20230514");
-    APPDEBUGLOG("call makeOrder success");
+    // APPDEBUGLOG("call makeOrder success");
     if (done) {
       done->Run();
       delete done;
@@ -50,25 +50,24 @@ class OrderImpl : public Order {
 };
 
 
-int main(int argc, char* argv[]) {
+int main() {
 
-  if (argc != 2) {
-    printf("Start test_rpc_server error, argc not 2 \n");
-    printf("Start like this: \n");
-    printf("./test_rpc_server ../conf/rocket.xml \n");
-    return 0;
-  }
+  // if (argc != 2) {
+  //   printf("Start test_rpc_server error, argc not 2 \n");
+  //   printf("Start like this: \n");
+  //   printf("./test_rpc_server ../conf/hl.xml \n");
+  //   return 0;
+  // }
 
-  rocket::Config::SetGlobalConfig(argv[1]);
-
-  rocket::Logger::InitGlobalLogger();
+  hl::Config::SetGlobalConfig("/home/hl/hl-tinyrpc/conf/hl.xml");
+  hl::Logger::InitGlobalLogger();
 
   std::shared_ptr<OrderImpl> service = std::make_shared<OrderImpl>();
-  rocket::RpcDispatcher::GetRpcDispatcher()->registerService(service);
+  hl::RpcDispatcher::GetRpcDispatcher()->registerService(service);
 
-  rocket::IPNetAddr::s_ptr addr = std::make_shared<rocket::IPNetAddr>("127.0.0.1", rocket::Config::GetGlobalConfig()->m_port);
+  hl::IPNetAddr::s_ptr addr = std::make_shared<hl::IPNetAddr>("127.0.0.1",12346);
 
-  rocket::TcpServer tcp_server(addr);
+  hl::TcpServer tcp_server(addr);
 
   tcp_server.start();
 
